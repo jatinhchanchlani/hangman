bead52a50268ea6cc49e24148b2d972e37b3d00e
jatinhchanchlani <jatin@Jatins-MacBook-Pro.local>
2020-10-25 21:54:13 -0700

    Updates

---
+++ precache-manifest.10a79f73c846da7f4838cbc4cde36f73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ea8dec1196a90843ab67f65c094cb0",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "67d8c23835f91d0ba67a",
+    "url": "/hangman/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "e76964b01ae68cb08aec",
+    "url": "/hangman/static/css/main.4a7c28dc.chunk.css"
+  },
+  {
+    "revision": "67d8c23835f91d0ba67a",
+    "url": "/hangman/static/js/2.cb43b2d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.cb43b2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76964b01ae68cb08aec",
+    "url": "/hangman/static/js/main.2c497b8e.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  },
+  {
+    "revision": "4fb9bcbbff51adb1e5570f7f2f9451c1",
+    "url": "/hangman/static/media/game_snapshot.4fb9bcbb.jpg"
+  }
+]);
